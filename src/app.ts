ef4c60b861094668b1227ce41a7525a470cebd3a
Dark.W <pustinia@gmail.com>
2023-02-08 01:14:52 +0900

    Add Dockerfile for docker build Fixes #6 - add simple http logic - modify git branch command

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+import { createServer } from "http";
+import { join } from "path";
+import * as fs from "fs";
 
 /**
  * @descriptions sample function for jest
@@ -16,5 +19,21 @@ type objectPlayer = {
 function greet(person: objectPlayer) {
     return "Hello there !! " + person.name;
   }
+
+const STATIC_PATH: string = "app";
+const PORT: number = 8081;
+
+// http server listen
+createServer((req, res) => {
+    const url = req.url === "/" ? "/index.html" : req.url;
+    const filePath = join(STATIC_PATH, `${url}`);
+    try {
+        const data = fs.readFileSync(filePath);
+        res.end(data);
+    } catch (err) {
+        res.statusCode = 404;
+        res.end(`File "${url}" is not found`);
+    }
+}).listen(PORT, () => console.log(`Static on port ${PORT}`));
 
 console.log(`test sample typescript, nodemon`);
